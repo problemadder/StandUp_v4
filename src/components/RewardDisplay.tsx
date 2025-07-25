5eea5ea49da8b9f8b68cec1e5d752d720cd3f949
problemadder <49922107+problemadder@users.noreply.github.com>
2025-07-25 22:12:41 +0200

    [dyad] wrote 1 file(s)

--- src/components/RewardDisplay.tsx
+++ src/components/RewardDisplay.tsx
@@ -51,10 +51,10 @@ const RewardDisplay: React.FC<RewardDisplayProps> = ({ reward }) => {
           <div className="flex flex-col items-center">
             <p className="font-semibold mb-4 text-center">Wozu gehört diese Flagge?</p>
             <img
-              src={`https://flagcdn.com/72x54/${flagReward.flagCode}.png`}
-              srcSet={`https://flagcdn.com/144x108/${flagReward.flagCode}.png 2x, https://flagcdn.com/216x162/${flagReward.flagCode}.png 3x`}
-              width="72"
-              height="54"
+              src={`https://flagcdn.com/144x108/${flagReward.flagCode}.png`}
+              srcSet={`https://flagcdn.com/288x216/${flagReward.flagCode}.png 2x, https://flagcdn.com/384x288/${flagReward.flagCode}.png 3x`}
+              width="144"
+              height="108"
               alt={`Flagge von ${flagReward.countryName}`}
               className="mb-4 border border-border shadow-md"
             />
